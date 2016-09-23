9a93fb2a3308b3c5ceef8a3ecf854f226a8246c2
zaelthan <ikubaczi@gmail.com>
2016-09-23 19:32:19 +0200

    Add files via upload

---
+++ Napiwek w restauracji.py
@@ -0,0 +1,7 @@
+"""
+This program counts the tip of a meal that is equal to 15% of the meal.
+"""
+print ('What is the cost of the whole meal? (no change)')
+meal = input() #Person puts the cost of the meal there
+
+print ('Your tip should be: ' + str(int(meal) * 0.15) +' dollars.') #Prints the tip that you have to give in a restaurant.
